7b5f285f6f2f645ea7a004f1b4397d841e8e86e9
Jose Miguel Garcia <jmgbenayas@gmail.com>
2018-03-07 15:51:06 +0100

    Añadida la descarga de ficheros y creacion de la carpeta downloads

--- src/filedownloader/FileDownloader.java
+++ src/filedownloader/FileDownloader.java
@@ -1,21 +1,37 @@
 package filedownloader;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class FileDownloader {
     private static final String downloadFilePath = "https://github.com/jesussanchezoro/PracticaPC/raw/master/descargas.txt";
+    private static final String pathOutput = "downloads";
 
     public FileDownloader() {
-        throw new RuntimeException("Not yet implemented");
+        new File(pathOutput).mkdirs();
     }
 
-    public void process(String downloadsFileURL) {
-        throw new RuntimeException("Not yet implemented");
+    public static void process(String downloadsFileURL) {
+        try {
+            URL website = new URL(downloadsFileURL);
+            InputStream in = website.openStream();
+            Path pathOut = Paths.get(pathOutput + "/" + "ficherodescargado.txt");
+            Files.copy(in, pathOut, StandardCopyOption.REPLACE_EXISTING);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static void main(String[] args) throws IOException {
-        String downloadFile = downloadFilePath;
         FileDownloader fd = new FileDownloader();
-        fd.process(downloadFile);
+        fd.process(downloadFilePath);
     }
 }
